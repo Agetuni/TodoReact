e85a54c96190d6ce6cea8d190a717f499b42bfa8
Aleazar yilma <yilma4060@gmail.com>
2022-09-12 18:12:16 +0300

    add lifecycle using react

--- react-todo-app/src/components/TodoContainer.js
+++ react-todo-app/src/components/TodoContainer.js
@@ -39,23 +39,7 @@ class TodoContainer extends React.Component {
     });
   };
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
   setUpdate = (updatedTitle, id) => {
     this.setState({
@@ -67,6 +51,25 @@ class TodoContainer extends React.Component {
       }),
     });
   };
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+    
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
   render() {
     return (
       <div className="container">
