e58c859731a9eeef639ff688aae2bda4a85dc50d
Aleazar yilma <yilma4060@gmail.com>
2022-09-12 20:31:22 +0300

    router

---
+++ src/functionBased/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+const links = [
+    {
+      id: 1,
+      path: "/",
+      text: "Home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      text: "About",
+    },
+  ]
+  export default function Navbar() {
+    return <>
+        <nav className="navBar">
+            <ul>
+                {links.map((link, id) =>
+                    <li key={id}>
+                        <NavLink
+                            to={link.path}
+                            style={({ isActive }) => isActive
+                                ? { color: 'orangered', textDecoration: 'underline' }
+                                : { color: 'black', textDecoration: 'none' }
+                            }
+                        >
+                            {link.text}
+                        </NavLink>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    </>
+}
